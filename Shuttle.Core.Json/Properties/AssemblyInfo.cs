d357a48b6ad8764ba37363b88954aa2b182b9f68
Eben <me@ebenroux.co.za>
2024-07-17 17:17:14 +0200

    Package upgrade

--- Shuttle.Core.Json/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Json/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("12.0.1.0")]
+[assembly: AssemblyVersion("12.0.2.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2024, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Json")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("12.0.1")]
+[assembly: AssemblyInformationalVersion("12.0.2")]
 [assembly: ComVisible(false)]
